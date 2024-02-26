c871113bfac354cd8a62a204a7dff149295a8b75
Muhammad Umer Farooq <digitel966@gmail.com>
2024-02-27 01:19:20 +0500

    update

--- Portfolio webiste/script.js
+++ Portfolio webiste/script.js
@@ -1,4 +1,6 @@
 let typewriter = document.getElementById("typewriter");
+let paragraph = document.getElementById("paragraph");
+let detail = document.getElementById("detail");
 
 var data = "Hello I'm Umer";
 
@@ -9,10 +11,24 @@ const sleep = (time) => {
 const doSomething = async () => {
   await sleep(1200);
   for (let i = 0; i < data.length; i++) {
-    var rand = Math.floor(Math.random() * 600) + 200;
+    var rand = Math.floor(Math.random() * 500) + 200;
     await sleep(rand);
     typewriter.innerHTML += data[i];
   }
+  await sleep(500);
+  paragraph.innerHTML = "Software ";
+  await sleep(500);
+  paragraph.innerHTML += " Engineer";
+  await sleep(500);
+  paragraph.className += "underline";
+
+  await sleep(1000);
+  detail.innerHTML =
+    "I'm an Software engineer focusing on web development and Social media Marketing <br>";
+  await sleep(500);
+  detail.innerHTML +=
+    " I'm providing freelance web development solutions to my clients as a Worpress expert and web-development expert.";
+  await sleep(500);
 };
 
 doSomething();
